ac210d1844efc1f7157748d691f839b2cce2a338
Louise Crow <louise.crow@gmail.com>
2010-09-28 08:56:16 +0100

    Don't use character encodings - they don't seem to work when used in javascript

--- lang/de.php
+++ lang/de.php
@@ -13,24 +13,24 @@
 
 
 $translations = array(
-	'Funnels_PluginDescription' => 'Erlaubt das Anlegen, die Analyse und Visualisierung von Funnels verkn&uuml;pft mit Zielen.',
+	'Funnels_PluginDescription' => 'Erlaubt das Anlegen, die Analyse und Visualisierung von Funnels verknüpft mit Zielen.',
   'Funnels_Funnels' => 'Funnels',
-	'Funnels_Overview' => '&Uuml;berblick',
-	'Funnels_NoFunnelsNeedAccess' => 'Nur ein Administrator oder Super User kann Funnels f&uuml;r eine definierte Website hinzuf&uuml;gen. Bitten Sie Ihren Piwik Administrator, einen Funnel f&uuml;r ein Ziel bzw. eine Website einzurichten.',
-  'Funnels_AddNewFunnel' => 'Neuen Funnel f&uuml;r ein Ziel hinzuf&uuml;gen',	
-	'Funnels_AddNewFunnelLink' => 'Neuen Funnel f&uuml;r ein Ziel hinzuf&uuml;gen',
+	'Funnels_Overview' => 'Überblick',
+	'Funnels_NoFunnelsNeedAccess' => 'Nur ein Administrator oder Super User kann Funnels für eine definierte Website hinzufügen. Bitten Sie Ihren Piwik Administrator, einen Funnel für ein Ziel bzw. eine Website einzurichten.',
+  'Funnels_AddNewFunnel' => 'Neuen Funnel für ein Ziel hinzufügen',	
+	'Funnels_AddNewFunnelLink' => 'Neuen Funnel für ein Ziel hinzufügen',
 	'Funnels_Funnel' => 'Funnel',
 	'Funnels_FunnelsManagement' => 'Funnels Management',
 	'Funnels_EditExistingFunnel' => 'Bearbeite existierende Ziel Funnels',
-	'Funnels_AddNewGoal' => 'Neues Ziel hinzuf&uuml;gen',
-	'Funnels_GoalsPluginDeactivated' => 'Um Funnels hinzuf&uuml;gen zu k&ouml;nnen, m&uuml;ssen Sie erst das Plugin \"Goals\" (Ziele) aktivieren.',
+	'Funnels_AddNewGoal' => 'Neues Ziel hinzufügen',
+	'Funnels_GoalsPluginDeactivated' => 'Um Funnels hinzufügen zu können, müssen Sie erst das Plugin \"Goals\" (Ziele) aktivieren.',
 	'Funnels_FunnelGoal' => 'Ziel',
-	'Funnels_AddFunnel_js' => 'Funnel hinzuf&uuml;gen',
+	'Funnels_AddFunnel_js' => 'Funnel hinzufügen',
 	'Funnels_UpdateFunnel_js' => 'Aktualisiere Funnel',
-	'Funnels_DeleteFunnelConfirm_js' => 'Sind Sie sicher, dass Sie diesen Funnel f&uuml;r %s l&ouml;schen m&ouml;chten?',
+	'Funnels_DeleteFunnelConfirm_js' => 'Sind Sie sicher, dass Sie diesen Funnel für %s löschen möchten?',
 	'Funnels_Step' => 'Schritt', 
 	'Funnels_StepUrl' => 'URL (z.B. "/step1.html")',
 	'Funnels_StepName' => 'Name',
 	'Funnels_FunnelConversionRate' => 'Funnel Conversion-Rate',
-	'Funnels_ContinuedToNextStep' => 'weiter zum n&auml;chsten Schritt'
+	'Funnels_ContinuedToNextStep' => 'weiter zum nächsten Schritt'
 );
